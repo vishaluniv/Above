de932079e73464cdcfc286ec47a81de451ce00ca
vishaluniv <103934678+vishaluniv@users.noreply.github.com>
2023-03-11 09:52:18 +0530

    Create nav.js

---
+++ Topic2/nav.js
@@ -0,0 +1,50 @@
+
+$('#red1').on('click', function(){
+  document.getElementById("bulb3").src="red.png"
+
+});
+
+$('#green1').on('click', function(){
+  document.getElementById("bulb3").src="green.png"
+
+});
+
+$('#yellow1').on('click', function(){
+  document.getElementById("bulb3").src="yellow.png"
+
+});
+
+$('#red2').on('click', function(){
+  document.getElementById("bulb2").src="red.png"
+
+});
+
+$('#green2').on('click', function(){
+  document.getElementById("bulb2").src="green.png"
+
+});
+
+$('#yellow2').on('click', function(){
+  document.getElementById("bulb2").src="yellow.png"
+
+});
+
+$('#red3').on('click', function(){
+  document.getElementById("bulb1").src="red.png"
+
+});
+
+$('#green3').on('click', function(){
+  document.getElementById("bulb1").src="green.png"
+
+});
+
+$('#yellow3').on('click', function(){
+  document.getElementById("bulb1").src="yellow.png"
+
+});
+
+
+
+
+
